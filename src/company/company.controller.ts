537f1078d32851ade55f6dcd9af29a512374470f
sujung <elodiekim93@gmail.com>
2025-03-17 09:31:48 +0900

    test

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -51,6 +51,7 @@ export class CompanyController {
   // ─────────────────────────────────────────────────────────
   // ✅ 비지니스 권한 유저 등록한 회사 조회 API
   // ─────────────────────────────────────────────────────────
+  //
   @Get()
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard, RolesGuard)
