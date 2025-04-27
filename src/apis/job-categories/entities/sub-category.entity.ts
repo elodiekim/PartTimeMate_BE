b5310aee9a91ba33dd370980d53b72e4f6a37442
elodie <elodiekim93@gmail.com>
2025-04-27 22:59:49 +0900

    fix : job-category

--- src/apis/job-categories/entities/sub-category.entity.ts
+++ src/apis/job-categories/entities/sub-category.entity.ts
@@ -18,8 +18,8 @@ export class SubCategory {
   @Column({ length: 100 })
   name: string;
 
-  // @ManyToOne(() => JobCategory, (jobCategory) => jobCategory.subCategories)
-  // jobCategory: JobCategory;
+  @ManyToOne(() => JobCategory, (jobCategory) => jobCategory.subCategories)
+  jobCategory: JobCategory;
   @Column()
   jobCategoryId: number;
 
